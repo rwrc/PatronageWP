7d5275659dc984bad9db2515c97de6454993dcb8
YOUR NAME <rwrc@o2.pl>
2014-12-18 01:29:39 +0100

    initial project version

---
+++ PatronageWP/Place.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronageWP
+{
+    class Place:INotifyPropertyChanged
+    {
+        private string _name;
+        private string _address;
+        private double _latitude;
+        private double _longitude;
+        private bool _hasWifi;
+
+
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+                NotifyPropertyChanged("Address");
+            }
+        }
+        public double Latitude
+        {
+            get { return _latitude; }
+            set
+            {
+                _latitude = value;
+                NotifyPropertyChanged("Latitude");
+            }
+        }
+        public double Longitude
+        {
+            get { return _longitude; }
+            set
+            {
+                _longitude = value;
+                NotifyPropertyChanged("Longitude");
+            }
+        }
+        public bool HasWifi 
+        { 
+            get{return _hasWifi;}
+            set
+            {
+                _hasWifi = value;
+                NotifyPropertyChanged("HasWifi");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}",Name,Address);
+        }
+
+
+        protected void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+    }
+}
